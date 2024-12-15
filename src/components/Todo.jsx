c85dcc3b781c4d1bed369d26ef5ab31664bbec15
subhashinisai <subhashinisaipriyatripathi@gmail.com>
2024-12-15 20:56:00 +0530

    dynamic task addition and deletion

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClipboardList } from '@fortawesome/free-solid-svg-icons';
 import Todoitems from './Todoitems';
 
 const Todo = () => {
+    
+    const [todoList,setTodoList]= useState([]);
+
+    const inputRef = useRef();
+
+
+    const add = () => {
+     const inputText = inputRef.current.value.trim();
+       if(inputText===""){
+        return null;
+     }
+     const newTodo ={
+        id: Date.now(),
+        text: inputText,
+        isComplete: false,
+     }
+     setTodoList((prev)=>[...prev,newTodo]);
+     inputRef.current.value="";
+    };
+
+
+    const deleteTodo = (id) =>{
+      setTodoList((prvTodos)=>{
+        return prvTodos.filter((todo) => todo.id !=id)
+      })
+    }
+
+
+
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-2xl'>
 
@@ -21,8 +51,8 @@ const Todo = () => {
         {/* input */}
     <div className='flex items-center my-7 bg-gray-200 rounded-full'>
 
-        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
-        <button className='border-none rounded-full bg-orange-600 w-32 h-14 test-white text-lg font-medium cursor-pointer'>ADD +</button>
+        <input ref={ inputRef } className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
+        <button onClick={ add } className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
 
    </div>
         
@@ -30,8 +60,11 @@ const Todo = () => {
 
          {/* todo list */}
          <div>
-            <Todoitems/>
-            <Todoitems/>
+            
+            {todoList.map((item,index)=>{
+                return <Todoitems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} />
+            })}
+
          </div>
     </div>
   )
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
-const Todoitems = () => {
+const Todoitems = ({text , id , isComplete , deleteTodo}) => {
   return (
     <div className='flex items-center my-3 gap-2' >
 
         <div className='flex flex-1 items-center cursor-pointer'>
         <FontAwesomeIcon icon={faSquareCheck} size='lg' className='cursor-pointer text-orange-600' />
-        <p className='text-slate-700 ml-4 text-[17px]'>learn dsa</p>
+        <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
 
         </div>
-       <FontAwesomeIcon icon={faTrash} className='cursor-pointer text-orange-600'/>
+       <FontAwesomeIcon icon={faTrash} className='cursor-pointer text-orange-600' onClick={()=>{deleteTodo(id)}}/>
     </div>
   )
 }
